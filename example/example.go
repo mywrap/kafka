a4de9d95401d567168a037bcc91b8d65888f56f0
Đào Thanh Tùng <tung.dao@techx.vn>
2020-07-30 17:03:55 +0700

    example using different node address

--- example/example.go
+++ example/example.go
@@ -8,18 +8,19 @@ import (
 )
 
 func main() {
-	servers := "192.168.99.100:9092,192.168.99.101:9092,192.168.99.102:9092"
+	//"192.168.99.100:9092,192.168.99.101:9092,192.168.99.102:9092"
+
 	producer, err := kafka.NewProducer(kafka.ProducerConfig{
-		BrokersList:  servers,
+		BrokersList:  "192.168.99.100:9092,192.168.99.101:9092",
 		RequiredAcks: kafka.WaitForAll,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if false {
+	if true {
 		consumer, err := kafka.NewConsumer(kafka.ConsumerConfig{
-			BootstrapServers: servers,
+			BootstrapServers: "192.168.99.102:9092",
 			GroupId:          "group0",
 			Offset:           kafka.OffsetLatest,
 			Topics:           "topic0,topic1",
